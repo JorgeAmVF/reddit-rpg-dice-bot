ff3c61d96ebc0561c6f2977d5cc966641f1fde9f
JorgeAmVF <javf.pub@icloud.com>
2019-09-26 03:24:00 -0300

    Remove printed lines

--- bot.py
+++ bot.py
@@ -8,79 +8,46 @@ import random
 
 def __init__():
     r = praw.Reddit('bot')
-    print ('Logged.')
     return r
 
 
 def run_bot(r, dice_rolled):
-    print ("Searching last 1000 comments looking for players rolling dice...")
     for comment in r.subreddit('RPG_Dice_Bot').comments(limit=1000):
         if "RollD20!" in comment.body and comment.permalink not in dice_rolled:
-            print ("Rolled dice found in the following comment: " + comment.permalink + ".")
-            print ("The die is cast, " + comment.author.name + "!")
             comment.reply("*ALEA IACTA EST*: " + "🎲**" + str(random.randint(1,20)) + "**🎲")
-            print ("Die face successfully read!")
             dice_rolled.append(comment.permalink)
             with open ("dice_rolled.txt", "a") as f:
                 f.write(comment.permalink + "\n")
-                print ("The comment permalink has been archived to avoid duplicates in the future.")
         if "RollD12!" in comment.body and comment.permalink not in dice_rolled:
-            print ("Rolled dice found in the following comment: " + comment.permalink + ".")
-            print ("The die is cast, " + comment.author.name + "!")
             comment.reply("*ALEA IACTA EST*: " + "🎲**" + str(random.randint(1,12)) + "**🎲")
-            print ("Die face successfully read!")
             dice_rolled.append(comment.permalink)
             with open ("dice_rolled.txt", "a") as f:
                 f.write(comment.permalink + "\n")
-                print ("The comment permalink has been archived to avoid duplicates in the future.")
         if "RollD%!" in comment.body and comment.permalink not in dice_rolled:
-            print ("Rolled dice found in the following comment: " + comment.permalink + ".")
-            print ("The die is cast, " + comment.author.name + "!")
             comment.reply("*ALEA IACTA EST*: " + "🎲**" + str(random.randint(1,100)) + "**🎲")
-            print ("Die face successfully read!")
             dice_rolled.append(comment.permalink)
             with open ("dice_rolled.txt", "a") as f:
                 f.write(comment.permalink + "\n")
-                print ("The comment permalink has been archived to avoid duplicates in the future.")
         if "RollD10!" in comment.body and comment.permalink not in dice_rolled:
-            print ("Rolled dice found in the following comment: " + comment.permalink + ".")
-            print ("The die is cast, " + comment.author.name + "!")
             comment.reply("*ALEA IACTA EST*: " + "🎲**" + str(random.randint(0,9)) + "**🎲")
-            print ("Die face successfully read!")
             dice_rolled.append(comment.permalink)
             with open ("dice_rolled.txt", "a") as f:
                 f.write(comment.permalink + "\n")
-                print ("The comment permalink has been archived to avoid duplicates in the future.")
         if "RollD8!" in comment.body and comment.permalink not in dice_rolled:
-            print ("Rolled dice found in the following comment: " + comment.permalink + ".")
-            print ("The die is cast, " + comment.author.name + "!")
             comment.reply("*ALEA IACTA EST*: " + "🎲**" + str(random.randint(1,8)) + "**🎲")
-            print ("Die face successfully read!")
             dice_rolled.append(comment.permalink)
             with open ("dice_rolled.txt", "a") as f:
                 f.write(comment.permalink + "\n")
-                print ("The comment permalink has been archived to avoid duplicates in the future.")                    
         if "RollD6!" in comment.body and comment.permalink not in dice_rolled:
-            print ("Rolled dice found in the following comment: " + comment.permalink + ".")
-            print ("The die is cast, " + comment.author.name + "!")
             comment.reply("*ALEA IACTA EST*: " + "🎲**" + str(random.randint(1,6)) + "**🎲")
-            print ("Die face successfully read!")
             dice_rolled.append(comment.permalink)
             with open ("dice_rolled.txt", "a") as f:
                 f.write(comment.permalink + "\n")
-                print ("The comment permalink has been archived to avoid duplicates in the future.")                 
         if "RollD4!" in comment.body and comment.permalink not in dice_rolled:
-            print ("Rolled dice found in the following comment: " + comment.permalink + ".")
-            print ("The die is cast, " + comment.author.name + "!")
             comment.reply("*ALEA IACTA EST*: " + "🎲**" + str(random.randint(1,4)) + "**🎲")
-            print ("Die face successfully read!")
             dice_rolled.append(comment.permalink)
             with open ("dice_rolled.txt", "a") as f:
                 f.write(comment.permalink + "\n")
-                print ("The comment permalink has been archived to avoid duplicates in the future.")                 
-    print (dice_rolled)
-    print ("Task done!")
-    print ("Awaiting 10 seconds to restart...")
     time.sleep(10)
 
     
@@ -97,6 +64,5 @@ def get_saved_comments():
 
 r = __init__()
 dice_rolled = get_saved_comments()
-print (dice_rolled)
 while True:
     run_bot(r, dice_rolled)
